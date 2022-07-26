9c7dc49f73a9d71d6a4a7a09f5bbe995c8075814
Maria Grimaldi <maria.grimaldi@edunext.co>
2022-07-26 09:56:06 -0400

    Bump version: 0.2.0 → 0.2.1

--- openedx_cas/__init__.py
+++ openedx_cas/__init__.py
@@ -2,6 +2,6 @@
 Support for Certralized Authentication System (CAS) in the Open edX authentication flows.
 """
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 default_app_config = 'openedx_cas.apps.OpenedxCasConfig'  # pylint: disable=invalid-name
